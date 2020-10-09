9b9f14c944a7bc43cbc92170f1b7fc7bef3b2b0b
offcorse <57239657+offcorse@users.noreply.github.com>
2020-10-09 10:37:58 -0600

    cleaned up code

--- Disk_inventory_sp.sql
+++ Disk_inventory_sp.sql
@@ -28,60 +28,60 @@ ALTER ROLE db_datareader
 
 --create tables
 CREATE TABLE artist_type(
-	artist_type_id	INT NOT NULL PRIMARY KEY IDENTITY,
+	artist_type_id			INT NOT NULL PRIMARY KEY IDENTITY,
 	artist_description		NVARCHAR(60) NOT NULL
 );
 
 CREATE TABLE disk_type (
-	disk_type_id INT NOT NULL PRIMARY KEY IDENTITY,
-	disk_description NVARCHAR(60) NOT NULL
+	disk_type_id			INT NOT NULL PRIMARY KEY IDENTITY,
+	disk_description		NVARCHAR(60) NOT NULL
 );
 
 CREATE TABLE status (
-	status_id	INT NOT NULL PRIMARY KEY IDENTITY,
-	status_description NVARCHAR(60) NOT NULL
+	status_id				INT NOT NULL PRIMARY KEY IDENTITY,
+	status_description		NVARCHAR(60) NOT NULL
 );
 
 CREATE TABLE genre (
-	genre_id	INT NOT NULL PRIMARY KEY IDENTITY,
-	genre_description NVARCHAR(60) NOT NULL
+	genre_id				INT NOT NULL PRIMARY KEY IDENTITY,
+	genre_description		NVARCHAR(60) NOT NULL
 );
 
 CREATE TABLE borrower (
-	borrower_id	INT NOT NULL PRIMARY KEY IDENTITY,
-	first_name NVARCHAR(60) NOT NULL,
-	last_name NVARCHAR(60) NOT NULL,
-	phone_num VARCHAR(15) NOT NULL,
+	borrower_id				INT NOT NULL PRIMARY KEY IDENTITY,
+	first_name				NVARCHAR(60) NOT NULL,
+	last_name				NVARCHAR(60) NOT NULL,
+	phone_num				VARCHAR(15) NOT NULL,
 
 );
 
 CREATE TABLE artist (
-	artist_id	INT NOT NULL PRIMARY KEY IDENTITY,
-	artist_first_name NVARCHAR(60) NOT NULL,
-	artist_last_name NVARCHAR(60) NULL,
-	artist_type_id	INT NOT NULL REFERENCES artist_type(artist_type_id)
+	artist_id				INT NOT NULL PRIMARY KEY IDENTITY,
+	artist_first_name		NVARCHAR(60) NOT NULL,
+	artist_last_name		NVARCHAR(60) NULL,
+	artist_type_id			INT NOT NULL REFERENCES artist_type(artist_type_id)
 );
 
 CREATE TABLE disk (
-	disk_id			INT NOT NULL PRIMARY KEY IDENTITY,
-	disk_name		NVARCHAR(60) NOT NULL,
-	release_date	DATE NOT NULL,
-	genre_id		INT NOT NULL REFERENCES genre(genre_id),
-	status_id		INT NOT NULL REFERENCES status(status_id),
-	disk_type_id	INT NOT NULL REFERENCES  disk_type(disk_type_id)
+	disk_id				INT NOT NULL PRIMARY KEY IDENTITY,
+	disk_name			NVARCHAR(60) NOT NULL,
+	release_date		DATE NOT NULL,
+	genre_id			INT NOT NULL REFERENCES genre(genre_id),
+	status_id			INT NOT NULL REFERENCES status(status_id),
+	disk_type_id		INT NOT NULL REFERENCES  disk_type(disk_type_id)
 );
 
 CREATE TABLE disk_has_borrower (
-	borrower_id		INT NOT NULL REFERENCES borrower(borrower_id),
-	disk_id			INT NOT NULL REFERENCES disk(disk_id),
-	borrowed_date	DATETIME2 NOT NULL,
-	returned_date   DATETIME2 NULL
+	borrower_id			INT NOT NULL REFERENCES borrower(borrower_id),
+	disk_id				INT NOT NULL REFERENCES disk(disk_id),
+	borrowed_date		DATETIME2 NOT NULL,
+	returned_date		DATETIME2 NULL
 	PRIMARY KEY (borrower_id,disk_id)
 );
 
 CREATE TABLE disk_has_artist (
-	disk_id			INT NOT NULL REFERENCES disk(disk_id),
-	artist_id		INT NOT NULL REFERENCES artist(artist_id),
+	disk_id				INT NOT NULL REFERENCES disk(disk_id),
+	artist_id			INT NOT NULL REFERENCES artist(artist_id),
 	PRIMARY KEY (disk_id,artist_id)
 );
 
