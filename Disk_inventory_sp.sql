75e078c673a0421f13db33fbc15067cc3293388f
offcorse <57239657+offcorse@users.noreply.github.com>
2020-10-29 10:48:59 -0600

    Add ins, upd, del proc for artist, disk, borrorower

--- Disk_inventory_sp.sql
+++ Disk_inventory_sp.sql
@@ -3,7 +3,8 @@
 Date		programmer			description
 10/09/2020	Scott Pinkerton		Build disk_inventory_SP database and add login/user
 10/16/2020	Scott Pinkerton		Add insert statements for all tables
-10/23/2020	Scott Pinkerton		Added queries for customer. Disk and artist not matching up. 
+10/23/2020	Scott Pinkerton		Added queries for customer. 
+10/28/2020	Scott Pinkerton		Add ins, upd, del proc for artist, disk, borrorower
 */
 
 --create database
@@ -154,7 +155,7 @@ INSERT INTO [dbo].[borrower]
 		   ('Mickey','Mouse','222-222-1234'),
 		   ('Abel','Crowe','333-444-1345'),
 		   ('Bob','Crane','111-333-7654'),
-		   ('Carlos','Santana','666-333-7654'),
+		   ('Carlos','Hyde','666-333-7654'),
 		   ('Donald','Duck','777-333-7654'),
 		   ('Ethan','Hawke','888-333-7654'),
 		   ('Fred','Flinstone','999-333-7654'),
@@ -163,7 +164,7 @@ INSERT INTO [dbo].[borrower]
 		   ('Icabod','Crane','111-456-7654'),
 		   ('Joe','Blow','111-333-7654'),
 		   ('John','Beaver','111-444-7654'),
-		   ('Jim','Croce','111-555-7654'),
+		   ('Jim','Jones','111-555-7654'),
 		   ('Jeff','Bridges','111-666-7654'),
 		   ('Jimmy','Smits','111-777-7654'),
 		   ('Kelly','Rowe','111-888-7654'),
@@ -287,23 +288,23 @@ INSERT INTO [dbo].[disk_has_artist]
            (1,1)
 		   ,(2,1)
 		   ,(3,3)
-		   ,(4,4)
-		   ,(5,5)
-		   ,(6,6)
-		   ,(7,7)
-		   ,(8,8)
-		   ,(9,9)
-		   ,(10,10)
-		   ,(11,11)
-		   ,(12,12)
-		   ,(13,13)
-		   ,(14,14)
-		   ,(15,15)
-		   ,(16,16)
-		   ,(17,17)
-		   ,(18,18)
-		   ,(19,19)
-		   ,(20,20)
+		   ,(4,7)
+		   ,(5,9)
+		   ,(6,11)
+		   ,(7,11)
+		   ,(8,11)
+		   ,(9,14)
+		   ,(10,14)
+		   ,(11,20)
+		   ,(12,20)
+		   ,(13,20)
+		   ,(14,20)
+		   ,(15,13)
+		   ,(16,13)
+		   ,(17,13)
+		   ,(18,13)
+		   ,(19,13)
+		   ,(20,8)
 GO
 
 --part H
@@ -313,7 +314,7 @@ where returned_date is null;
 
 --project 4 starts here
 --3.Show the disks in your database and any associated Individual artists only.
-select disk_name as 'Disk Name', release_date as 'Release Date', artist.artist_first_name as 'ArtistFirstName' ,artist.artist_last_name as 'ArtistLastName'
+select disk_name as 'Disk Name', format(release_date,'MM/dd/yyyy') as 'Release Date', artist.artist_first_name as 'ArtistFirstName' ,artist.artist_last_name as 'ArtistLastName'
 from artist
 join disk_has_artist
 	on artist.artist_id = disk_has_artist.artist_id
@@ -328,7 +329,7 @@ go
 drop view if exists View_Individual_Artist
 go
 create view View_Individual_Artist as
-select artist_first_name as 'FirstName',artist_last_name as 'LastName'
+select artist.artist_id, artist_first_name as 'FirstName',artist_last_name as 'LastName'
 from artist
 join disk_has_artist
 	on disk_has_artist.artist_id = artist.artist_id
@@ -337,28 +338,29 @@ join disk
 where artist_type_id = 1
 go
 
-select * from View_Individual_Artist
+select FirstName,LastName from View_Individual_Artist
 
 --5.Show the disks in your database and any associated Group artists only. Use the View_Individual_Artist view.
 
-select disk_name as 'Disk Name', release_date as 'Release Date', artist_first_name as 'Group Name'
+select disk_name as 'Disk Name', format(release_date,'MM/dd/yyyy') as 'Release Date', artist_first_name as 'Group Name'
 from artist
 join disk_has_artist
 	on disk_has_artist.artist_id = artist.artist_id
 join disk
 	on disk_has_artist.disk_id = disk.disk_id
-where artist_type_id = 2
---could not figure out how to use the view
+where artist_first_name not in
+	(select FirstName from View_Individual_Artist)
+order by artist_first_name;
 go
 
 --6. Show the borrowed disks and who borrowed them.
-select first_name as First,last_name as Last, disk_name as 'Disk Name',borrowed_date as 'Borrowed Date',returned_date as 'Returned Date'
+select first_name as First,last_name as Last, disk_name as 'Disk Name',cast(borrowed_date as date) as 'Borrowed Date',cast(returned_date as date) as 'Returned Date'
 from disk
 join disk_has_borrower 
 	on disk.disk_id = disk_has_borrower.disk_id
 join borrower
 	on borrower.borrower_id = disk_has_borrower.borrower_id
-order by disk_name
+order by last_name
 
 --7. Show the number of times a disk has been borrowed.
 select disk.disk_id as DiskID, disk_name as 'Disk Name', COUNT(*) as TimesBorrowed
@@ -369,11 +371,202 @@ group by disk.disk_id, disk_name
 order by disk.disk_id
 
 --8. Show the disks outstanding or on-loan and who has each disk.
-select disk_name as 'Disk Name', borrowed_date as Borrowed, returned_date as Returned,last_name as LastName
+select disk_name as 'Disk Name', cast(borrowed_date as date) as Borrowed, returned_date as Returned,last_name as LastName
 from disk
 join disk_has_borrower
 	on disk.disk_id = disk_has_borrower.disk_id
 join borrower
 	on disk_has_borrower.borrower_id = borrower.borrower_id
 where returned_date is null
-order by last_name
+order by disk_name, last_name
+
+--project 5----------------------------------------------------------------
+USE [Disk_Inventory_SP]
+GO
+
+drop proc if exists sp_ins_disk;
+go
+create proc sp_ins_disk
+	@disk_name varchar(60), @release_date date, @genre_id int, @status_id int, @disk_type_id int
+as
+begin try
+	INSERT INTO [dbo].[disk] ([disk_name],[release_date],[genre_id],[status_id],[disk_type_id])
+		 VALUES
+			   (@disk_name,@release_date,@genre_id,@status_id,@disk_type_id);
+end try
+begin catch
+	print 'An error occured: ' + convert(varchar(200),Error_message());
+end catch
+go
+
+grant execute on sp_ins_disk to diskUsersp;
+
+exec sp_ins_disk 'Superunknown','3/3/1994',4,1,1;
+go
+--exec sp_ins_disk 'Superunknown3','3/3/1994',4,1,NULL;
+--go
+
+--update disk stored procedure
+drop proc if exists sp_upd_disk;
+go
+create proc sp_upd_disk
+	@disk_id int, @disk_name varchar(60), @release_date date, @genre_id int, @status_id int, @disk_type_id int
+as
+begin try
+	update disk
+	set disk_name = @disk_name,release_date = @release_date, genre_id = @genre_id, status_id = @status_id, disk_type_id = @disk_type_id
+	where disk_id = @disk_id;
+end try
+begin catch
+	print 'An error occured: ' + convert(varchar(200),Error_message());
+end catch
+
+grant execute on sp_upd_disk to diskUsersp;
+
+exec sp_upd_disk 21,'Ultramega OK','1/1/1999',4,1,1;
+go
+--exec sp_upd_disk 21,'Ultramega OK','1/1/1999',4,1,NULL;
+--go
+
+--delete stored procedure
+drop proc if exists sp_del_disk;
+go
+create proc sp_del_disk 
+	@disk_id int
+as
+begin try
+	DELETE FROM [dbo].[disk]
+		  WHERE disk_id = @disk_id
+end try
+begin catch
+	print 'An error occured: ' + convert(varchar(200),Error_message());
+end catch
+
+grant execute on sp_del_disk to diskUsersp;
+
+exec sp_del_disk 22;
+
+--insert stored procedure for artist
+drop proc if exists sp_ins_artist;
+go
+create proc sp_ins_artist
+	@artist_first_name nvarchar(60),@artist_last_name nvarchar(60),@artist_type_id int
+as
+begin try
+	INSERT INTO [dbo].[artist]
+			   ([artist_first_name]
+			   ,[artist_last_name]
+			   ,[artist_type_id])
+		 VALUES
+			   (@artist_first_name
+			   ,@artist_last_name
+			   ,@artist_type_id);
+end try
+begin catch
+	print 'An error occured: ' + convert(varchar(200),Error_message());
+end catch
+
+grant execute on sp_ins_artist to diskUsersp;
+
+exec sp_ins_artist Soundgarden,null,2
+--exec sp_ins_artist Soundgarden,null,null  --generate error
+
+--update artist stored procedure
+drop proc if exists sp_upd_artist;
+go
+
+create proc sp_upd_artist
+	@artist_id int, @artist_first_name nvarchar(60), @artist_last_name nvarchar(60),
+	@artist_type_id int
+as
+begin try
+	update artist
+	set
+	artist_first_name = @artist_first_name, artist_last_name = @artist_last_name, artist_type_id = @artist_type_id
+	where
+	artist_id = @artist_id
+end try
+begin catch
+	print 'An error occured: ' + convert(varchar(200),Error_message());
+end catch
+
+grant execute on sp_upd_artist to diskUsersp;
+
+exec sp_upd_artist 22,'Avenged Sevenfold',null,2
+--exec sp_upd_artist 22,'Avenged Sevenfold',null,null  --generate error
+
+-- delete artist stored procedure
+drop proc if exists sp_del_artist;
+go
+create proc sp_del_artist
+	@artist_id int
+as
+begin try
+	DELETE FROM [dbo].[artist]
+		  WHERE artist_id = @artist_id
+end try
+begin catch
+	print 'An error occured: ' + convert(varchar(200),Error_message());
+end catch
+
+grant execute on sp_del_artist to diskUsersp;
+
+exec sp_del_artist 22;
+
+--insert borrower table stored procedure
+drop proc if exists sp_upd_borrower;
+go
+create proc sp_upd_borrower
+	@borrower_id int, @first_name nvarchar(60), @last_name nvarchar(60), @phone_num varchar(15)
+as
+begin try
+	update borrower
+	set first_name = @first_name, last_name = @last_name, phone_num = @phone_num
+	where borrower_id = @borrower_id
+end try
+begin catch
+	print 'An error occured: ' + convert(varchar(200),Error_message());
+end catch
+grant execute on sp_upd_borrower to diskUsersp;
+exec sp_upd_borrower 1,'John','Doe','222-222-2222'
+--exec sp_upd_borrower 1,null,null,null --generate error
+
+--insert borrower table stored procedure
+drop proc if exists sp_ins_borrower
+go
+create proc sp_ins_borrower
+	@first_name nvarchar(60), @last_name nvarchar(60), @phone_num varchar(15)
+as
+begin try
+
+	INSERT INTO [dbo].[borrower]
+           ([first_name]
+           ,[last_name]
+           ,[phone_num])
+     VALUES
+           (@first_name
+           ,@last_name
+           ,@phone_num);
+end try
+begin catch
+	print 'An error occured: ' + convert(varchar(200),Error_message());
+end catch
+grant execute on sp_ins_borrower to diskUsersp;
+exec sp_ins_borrower 'Jiminy','Cricket','123-333-4554'
+--exec sp_ins_borrower null,null,null  --generate error
+
+-- delete borrower table stored procedure
+drop proc if exists sp_del_borrower
+go
+create proc sp_del_borrower
+	@borrower_id int
+as
+begin try
+	delete from borrower
+	where borrower_id = @borrower_id
+end try
+begin catch
+	print 'An error occured: ' + convert(varchar(200),Error_message());
+end catch
+grant execute on sp_del_borrower to diskUsersp;
+exec sp_del_borrower 21
